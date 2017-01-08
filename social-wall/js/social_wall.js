532f47ed309b22a29a76b8d8f14208315f2cbe00
sushma <kukkadapusushma12@gmail.com>
2017-01-08 18:20:50 +0530

    set up correct url

--- social-wall/js/social_wall.js
+++ social-wall/js/social_wall.js
@@ -43,16 +43,15 @@ $(function() {
 
 $('#search-but').click(function(event){
   var query = $("#query").val();
-  query = '/api/search.json?q=' + encodeURIComponent(query);
+  query = 'http://api.loklak.org/api/search.json?q=' + encodeURIComponent(query);
 
   $("#masonry").empty();
   $("#masonry").html("<div class='item-size'></div>");
   $('#loadingmessage').show();
 
-  $.getJSON( 
-    query       
-  )
-  .done(function(data){
+  $.getJSON(
+	query+"&timezoneOffset="+(new Date()).getTimezoneOffset()+"&callback=?",
+	function(data){
 
     $('#loadingmessage').hide();
     
