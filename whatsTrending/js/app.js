275849ef4d291273e58b9044b8f94ef99d310489
Deepjyoti Mondal <djmdeveloper060796@gmail.com>
2017-05-06 22:19:12 +0530

    fixes #146 - Add proper error notification to whatsTrending? app
    Added error notification to whatsTrending app.
    Solves issue #146
    If user enters anything apart from date or date in
    wrong format then user will see error.

--- whatsTrending/js/app.js
+++ whatsTrending/js/app.js
@@ -3,7 +3,75 @@ app.controller('getHash', ['$scope', '$http', '$q', '$sce', function($scope, $ht
     $scope.queryUrl = "http://loklak.org/api/search.json?callback=JSON_CALLBACK&" +
                       "q=since:2016-06-01&source=cache&count=0&fields=hashtags&limit=10";
     $scope.count = 10;
-    $scope.start = "2016-06-01";
+    $scope.startdate = "2016-06-01";
+
+    $scope.isValidDate = function(dateString) {
+        var regEx = /^\d{4}-\d{2}-\d{2}$/;
+        if (dateString.match(regEx) === null) {
+            return false;
+        }
+
+        dateComp = dateString.split('-');
+        var i=0;
+        for (i=0; i<dateComp.length; i++) {
+            dateComp[i] = parseInt(dateComp[i]);
+        }
+
+        if (dateComp.length > 3) {
+            return false;
+        }
+
+        if (dateComp[1] > 12 || dateComp[1] <= 0) {
+            return false;
+        }
+
+        if (dateComp[2] > 31 || dateComp[2] <= 0) {
+            return false;
+        }
+
+        if (((dateComp[1] === 4) || (dateComp[1] === 6) || (dateComp[1] === 9) ||
+            (dateComp[1] === 11)) && (dateComp[2] > 30)) {
+            return false;
+        }
+
+        if (dateComp[1] ===2) {
+            if (((dateComp[0] % 4 === 0) && (dateComp[0] % 100 !== 0)) || (dateComp[0] % 400 === 0)) {
+                if (dateComp[2] > 29) {
+                    return false;
+                }
+            } else {
+                if (dateComp[2] > 28) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    $scope.isNumber = function(numString) {
+        var regEx = /^[0-9]+$/;
+        return String(numString).match(regEx) != null;
+    }
+
+    $scope.displayErrorModal = function(val, type) {
+        if (type === 0) {
+            if (!$scope.isValidDate(val)) {
+                $scope.loading = false;
+                $('.modal-body').html('<p> Please enter valid date in YYYY-MM-DD format </p>');
+                $('#myModal').modal('show');
+                return false;
+            }
+        } else {
+            if (!$scope.isNumber(val)) {
+                $scope.loading = false;
+                $('.modal-body').html('<p> Please enter a valid number </p>');
+                $('#myModal').modal('show');
+                return false;
+            }
+        }
+        return true;
+    }
 
     $scope.getTags = function() {
         $("#filter-options").slideToggle("fast");
@@ -12,16 +80,29 @@ app.controller('getHash', ['$scope', '$http', '$q', '$sce', function($scope, $ht
         var until = "&";
         var count = "limit=10";
 
-        if ($scope.startdate !== undefined) {
+        if ($scope.startdate !== undefined && $scope.startdate !== "") {
+            if (!$scope.displayErrorModal($scope.startdate, 0)) {
+                return;
+            }
             since = "since:" + $scope.startdate;
+        } else {
+            $scope.startdate = "2016-06-01";
         }
 
         if ($scope.enddate !== undefined) {
+            if (!$scope.displayErrorModal($scope.enddate, 0)) {
+                return;
+            }
             until = "%20until:" + $scope.enddate + "&";
         }
 
-        if ($scope.count !== undefined) {
+        if ($scope.count !== undefined && $scope.count !== "") {
+            if (!$scope.displayErrorModal($scope.count, 1)) {
+                return;
+            }
             count = "limit=" + $scope.count;
+        } else {
+            $scope.count = 10;
         }
 
         $scope.queryUrl = "http://loklak.org/api/search.json?callback=JSON_CALLBACK&" +
