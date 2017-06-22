dbdad31be905132d78093b6b291194b2a79ce8ee
Kavitha Nair <kavitha.nair09@gmail.com>
2017-06-22 22:09:58 +0530

    Fixes #204: Added easier way to choose emoji

--- emojiHeatmapper/script.js
+++ emojiHeatmapper/script.js
@@ -1,13 +1,24 @@
 //Loading the dropdown
-$(document).ready(function(){
-  var listItems= "";
-  $.getJSON("emoji.json", function(json) {
-    for (var i = 0; i < json.data.length; i++){
-      listItems+= "<option value='" + json.data[i][1] + "'>" + json.data[i][1] + "</option>";
-    }
-    $("#searchField").html(listItems);
+$(function() {
+  // Initializes and creates emoji set from sprite sheet
+  window.emojiPicker = new EmojiPicker({
+    emojiable_selector: '[data-emojiable=true]',
+    assetsPath: './lib/img/',
+    popupButtonClasses: 'fa fa-smile-o'
   });
+  // Finds all elements with `emojiable_selector` and converts them to rich emoji input fields
+  // You may want to delay this step if you have dynamically created input fields that appear later in the loading process
+  // It can be called as many times as necessary; previously converted input fields will not be converted again
+  window.emojiPicker.discover();
 });
+
+      // Google Analytics
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+ga('create', 'UA-49610253-3', 'auto');
+ga('send', 'pageview');
 
 // Geometries
 var point = new ol.geom.Point(
